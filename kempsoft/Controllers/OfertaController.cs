fe0be8adf5afdb4e4fcda0e02b20a92f16211284
fasgetz <fasgetz@yandex.ru>
2021-10-06 15:51:31 +0300

    Добавлена страница контактов и оферты в отношении обработки персональных данных

---
+++ kempsoft/Controllers/OfertaController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace kempsoft.Controllers
+{
+    public class OfertaController : Controller
+    {
+
+        public IActionResult PrivacyDataOferta()
+        {
+            return View();
+        }
+
+    }
+}
--- kempsoft/Startup.cs
+++ kempsoft/Startup.cs
@@ -23,7 +23,12 @@ namespace kempsoft
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            services.AddControllersWithViews().AddRazorRuntimeCompilation()
+                .AddNewtonsoftJson(options =>
+                options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
+            );
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
