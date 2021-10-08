e9cda1f7a0363179a2448ea5390d8d82db3178f1
fasgetz <fasgetz@yandex.ru>
2021-10-08 06:05:50 +0300

    Подключена база данных (в конфигурации данные для подключения)

---
+++ kempsoft/Models/DataBase/ReadyProject.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace kempsoft.Models.DataBase
+{
+    public partial class ReadyProject
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string TasksProject { get; set; }
+        public string LogoImg { get; set; }
+        public string ProjectRoute { get; set; }
+        public string ProjectImg { get; set; }
+    }
+}
---
+++ kempsoft/Models/DataBase/softkempContext.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace kempsoft.Models.DataBase
+{
+    public partial class softkempContext : DbContext
+    {
+        public softkempContext()
+        {
+        }
+
+        public softkempContext(DbContextOptions<softkempContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<ReadyProject> ReadyProjects { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "Cyrillic_General_CI_AS");
+
+            modelBuilder.Entity<ReadyProject>(entity =>
+            {
+                entity.Property(e => e.LogoImg)
+                    .HasMaxLength(100)
+                    .HasColumnName("logoImg");
+
+                entity.Property(e => e.Name).HasMaxLength(50);
+
+                entity.Property(e => e.ProjectImg).HasMaxLength(100);
+
+                entity.Property(e => e.ProjectRoute).HasMaxLength(50);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
